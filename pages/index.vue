5927125177cfda1ebd95819681ee08c91dee1d15
Jason Victorio <jgvictorio@gmail.com>
2022-03-16 16:55:52 +1300

    Display block component in home

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,72 @@
 <template>
-  <h1>Home</h1>
+  <div class="home">
+    <div class="home-header">
+      <h1 class="home-title">{{ title }}</h1>
+      <p class="home-description">{{ description }}</p>
+    </div>
+    <div class="home-blocks">
+      <block-component
+        v-for="block in blocks"
+        :key="block.title"
+        :title="block.title"
+        :description="block.description"
+      ></block-component>
+    </div>
+  </div>
 </template>
 
 <script>
+import BlockComponent from '~/components/BlockComponent.vue'
+
 export default {
   name: 'IndexPage',
+  components: { BlockComponent },
+  data: () => ({
+    title: 'Experienced & Multi-Talented',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor leo, portitor vel dictum at, aliquam et augeue. Duis vel molestie risus, ut lobortis felis.',
+    blocks: [
+      {
+        title: 'Block',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagit-tis nisl feugiat quis.',
+      },
+      {
+        title: 'Block',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagit-tis nisl feugiat quis.',
+      },
+      {
+        title: 'Block',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagit-tis nisl feugiat quis.',
+      },
+      {
+        title: 'Block',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagit-tis nisl feugiat quis.',
+      },
+      {
+        title: 'Block',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagit-tis nisl feugiat quis.',
+      },
+      {
+        title: 'Block',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagit-tis nisl feugiat quis.',
+      },
+      {
+        title: 'Block',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagit-tis nisl feugiat quis.',
+      },
+      {
+        title: 'Block',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagit-tis nisl feugiat quis.',
+      },
+    ],
+  }),
 }
 </script>

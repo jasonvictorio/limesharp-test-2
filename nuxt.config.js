3446bf086e35098ddf789173c1517d43d5f1c17c
Jason Victorio <jgvictorio@gmail.com>
2022-03-16 20:02:56 +1300

    Add gh-pages deploy script

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/limesharp-test-2/',
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
